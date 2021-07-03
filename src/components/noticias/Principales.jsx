419519ea26db20d47a7e1262060495e53928c481
Matias <matiasalarcon55@hotmail.com>
2021-07-02 23:14:20 -0300

    Noticias desde un JSON

--- src/components/noticias/Principales.jsx
+++ src/components/noticias/Principales.jsx
@@ -1,27 +1,14 @@
+import noticias from './noticias.json';
+
 const Principales = () => {
     return (
         <>
-            <div className='noticia'>
-                <h3>Título #1</h3>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste aut officia aliquam id, libero temporibus expedita ex laboriosam neque
-                    laborum perspiciatis eaque qui impedit nesciunt voluptatum provident veniam magni harum?
-                </p>
-            </div>
-            <div className='noticia'>
-                <h3>Título #1</h3>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste aut officia aliquam id, libero temporibus expedita ex laboriosam neque
-                    laborum perspiciatis eaque qui impedit nesciunt voluptatum provident veniam magni harum?
-                </p>
-            </div>
-            <div className='noticia'>
-                <h3>Título #1</h3>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste aut officia aliquam id, libero temporibus expedita ex laboriosam neque
-                    laborum perspiciatis eaque qui impedit nesciunt voluptatum provident veniam magni harum?
-                </p>
-            </div>
+            {noticias.slice(noticias.length - 3).map((noticia) => (
+                <div key={noticia.id} className='noticia'>
+                    <h3>{noticia.title}</h3>
+                    <p>{noticia.subtitle}</p>
+                </div>
+            ))}
         </>
     );
 };
