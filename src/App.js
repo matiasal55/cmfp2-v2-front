4330a69ee347b6f67bd6e3a161b2fbc2886dbc2a
Matias <matiasalarcon55@hotmail.com>
2021-07-03 23:40:09 -0300

    Pagina Nosotros

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
 import Index from './pages/Index';
+import Nosotros from './pages/Nosotros';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Layout>
                 <Switch>
-                    <Route path='/' component={Index} />
+                    <Route exact path='/' component={Index} />
+                    <Route exact path='/nosotros' component={Nosotros} />
                 </Switch>
             </Layout>
         </BrowserRouter>
