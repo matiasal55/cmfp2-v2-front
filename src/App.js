7ce9ab3055a9897ea00c69bdaa3dd23a5f2efb0d
Matias <matiasalarcon55@hotmail.com>
2021-07-04 22:46:19 -0300

    Pagina inscripcion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
 import Index from './pages/Index';
+import Inscripcion from './pages/Inscripcion';
 import Nosotros from './pages/Nosotros';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
             <Layout>
                 <Switch>
                     <Route exact path='/' component={Index} />
+                    <Route exact path='/inscripcion' component={Inscripcion} />
                     <Route exact path='/nosotros' component={Nosotros} />
                 </Switch>
             </Layout>
