bec9c4bf75c8f0afc1ba21a1387558064968b78f
Matias <matiasalarcon55@hotmail.com>
2021-07-05 13:48:38 -0300

    Fondo Inscripcion

--- src/pages/Inscripcion.jsx
+++ src/pages/Inscripcion.jsx
@@ -3,11 +3,12 @@ import Section from '../components/Section';
 import Informacion from '../components/inscripcion/Informacion';
 import Requisitos from '../components/inscripcion/Requisitos';
 import '../styles/pages/_inscripcion.scss';
+import background from '../assets/background/inscripcion.jpg';
 
 const Inscripcion = () => {
     return (
         <>
-            <Main />
+            <Main bgImage={background} />
             <Section>
                 <h1>Inscripción</h1>
                 <h2>Próxima fecha de inscripción: del 00 de LoremIpsum al 00 de LoremIpsum de 2020.</h2>
