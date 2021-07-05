d2426be287925ed890c479ca841c6b497c442570
Matias <matiasalarcon55@hotmail.com>
2021-07-05 13:45:56 -0300

    Scroll to top

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Layout from './components/Layout';
 import Index from './pages/Index';
 import Inscripcion from './pages/Inscripcion';
@@ -7,6 +8,7 @@ import Nosotros from './pages/Nosotros';
 const App = () => {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Layout>
                 <Switch>
                     <Route exact path='/' component={Index} />
