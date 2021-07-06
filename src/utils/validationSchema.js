e5525abf30c91447e24d594be276a7eb830fb822
Matias <matiasalarcon55@hotmail.com>
2021-07-05 21:56:03 -0300

    Width de textarea para mobile, trim en validation y espacios en text input

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,20 +1,22 @@
 import * as yup from 'yup';
 
 const validateText = (msjError, min = 3, max = 20) => {
-    const matches = /^[A-Za-z]+$/;
+    const matches = /^[A-Za-z ]+$/;
     return yup
         .string()
+        .trim()
         .required(msjError)
         .min(min, `El campo debe tener más de ${min} caracteres`)
         .max(max, `El campo debe tener menos de ${max} caracteres`)
         .matches(matches, 'Formato de texto inválido');
 };
 
-const validateEmail = () => yup.string().email('El formato de email no es válido').required('Ingrese un email');
+const validateEmail = () => yup.string().trim().email('El formato de email no es válido').required('Ingrese un email');
 
 const validateTel = () =>
     yup
         .string()
+        .trim()
         .required('Por favor ingrese un número de teléfono')
         .min(10, 'Ingrese un teléfono correcto. No olvide el código de área')
         .max(15, 'La cantidad de dígitos es mayor a la permitida');
