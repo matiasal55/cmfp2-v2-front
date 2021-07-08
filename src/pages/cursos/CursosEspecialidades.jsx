abd15ae8fa2530d38e56d411b2f56493802fa3ff
Matias <matiasalarcon55@hotmail.com>
2021-07-08 18:59:16 -0300

    Aplicacion de spinner en cursos especialidades. A futuro recurrira al backend

--- src/pages/cursos/CursosEspecialidades.jsx
+++ src/pages/cursos/CursosEspecialidades.jsx
@@ -3,15 +3,26 @@ import Section from '../../components/Section';
 import background from '../../assets/background/especialidades.jpg';
 import cursos from './especialidades.json';
 import Lista from '../../components/cursos/Lista';
+import { useState, useEffect } from 'react';
+import Spinner from '../../components/Spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 const CursosEspecialidades = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, []);
+
     return (
         <>
             <Main bgImage={background} />
             <Section>
                 <h1>Cursos Especialidades</h1>
                 <h3>Cursos con una duración de 4 meses a 2 años. Una vez finalizado ya podés trabajar de lo que aprendiste.</h3>
-                <Lista cursos={cursos} />
+                {loading ? <Spinner /> : <Lista cursos={cursos} />}
             </Section>
         </>
     );
