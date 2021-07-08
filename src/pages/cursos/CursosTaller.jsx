318759b0071d9cbf233c245032e428cf645477af
Matias <matiasalarcon55@hotmail.com>
2021-07-08 16:11:31 -0300

    Link borrado en CursosTaller

--- src/pages/cursos/CursosTaller.jsx
+++ src/pages/cursos/CursosTaller.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import Main from '../../components/Main';
 import Section from '../../components/Section';
 import background from '../../assets/background/especialidades.jpg';
@@ -10,7 +9,6 @@ const CursosTaller = () => {
             <Section>
                 <h1>Cursos Taller</h1>
                 <h3>Talleres de corta duración. Con posibilidades de salida laboral.</h3>
-                <Link to='/cursos-taller/5'>Prueba</Link>
             </Section>
         </>
     );
