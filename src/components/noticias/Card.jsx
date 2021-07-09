307b76184819e8969d8731a78770159dcbafd5fa
Matias <matiasalarcon55@hotmail.com>
2021-07-08 21:15:18 -0300

    Link a la noticia

--- src/components/noticias/Card.jsx
+++ src/components/noticias/Card.jsx
@@ -1,17 +1,20 @@
+import { Link } from 'react-router-dom';
 import '../../styles/noticias/_card.scss';
 
 const Card = ({ noticia }) => {
     const fecha = new Date();
 
     return (
-        <div className='noticia'>
-            <div className='noticia-img' style={{ background: `url(${noticia.mainImage})` }} />
-            <div className='detalles'>
-                <h2>{noticia.title}</h2>
-                <h3>{noticia.subtitle}</h3>
-                <small>Publicado el {fecha.toLocaleDateString()}</small>
+        <Link to={`/noticias/${noticia.id}-${noticia.title.replace(/ /g, '-')}`}>
+            <div className='noticia'>
+                <div className='noticia-img' style={{ background: `url(${noticia.mainImage})` }} />
+                <div className='detalles'>
+                    <h2>{noticia.title}</h2>
+                    <h3>{noticia.subtitle}</h3>
+                    <small>Publicado el {fecha.toLocaleDateString()}</small>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
