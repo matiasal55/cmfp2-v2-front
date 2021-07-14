7e950f543dace6c14fa6aacc509f27d019146f90
Matias <matiasalarcon55@hotmail.com>
2021-07-14 17:46:22 -0300

    Mejora de codigo para poder obtener los cursos tallr cuando se implementen

--- src/components/cursos/getCursos.js
+++ src/components/cursos/getCursos.js
@@ -1,9 +1,19 @@
 import { getHandler } from '../../utils/requestHandler';
 
 export const especialidades = async () => {
+    const lista = await getData('especialidades');
+    return lista;
+};
+
+export const taller = async () => {
+    const lista = await getData('taller');
+    return lista;
+};
+
+const getData = async (section) => {
     let lista;
     try {
-        lista = await getHandler('/cursos/especialidades');
+        lista = await getHandler('/cursos/' + section);
     } catch (e) {
         lista = [];
     } finally {
