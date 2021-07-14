11129ccad9ec56e90e1454401230a1902538c3b8
Matias <matiasalarcon55@hotmail.com>
2021-07-14 19:24:46 -0300

    Info de curso obtenido de la API

--- src/pages/cursos/Template.jsx
+++ src/pages/cursos/Template.jsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
 import Main from '../../components/Main';
 import Section from '../../components/Section';
-import cursos from '../../components/cursos/especialidades.json';
 import Contenidos from '../../components/cursos/Contenidos';
 import Presentacion from '../../components/cursos/Presentacion';
 import Requisitos from '../../components/cursos/Requisitos';
@@ -9,26 +8,47 @@ import Duracion from '../../components/cursos/Duracion';
 import Inscripcion from '../../components/cursos/Inscripcion';
 import Certificacion from '../../components/cursos/Certificacion';
 import Dias from '../../components/cursos/Dias';
+import { getCurso } from '../../components/cursos/getCursos';
 import '../../styles/components/_items.scss';
+import { useEffect, useState } from 'react';
+import Spinner from '../../components/Spinner';
 
-const Template = () => {
+const Template = ({ section }) => {
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
     const { curso } = useParams();
     const id = parseInt(curso.substring(0, curso.indexOf('-')));
-    let data = cursos.filter((curso) => curso.id === id);
-    data = data[0];
+
+    const request = async (section, id) => {
+        const curso = await getCurso(section, id);
+        setData(curso);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        request(section, id);
+    }, []);
 
     return (
         <>
             <Main bgImage={data.imgMain} />
             <Section>
-                <h1>{data.titulo}</h1>
-                {data.presentacion ? <Presentacion texto={data.presentacion} /> : null}
-                <Contenidos contenidos={data.contenidos} />
-                <Requisitos requisitos={data.estudios} />
-                <Duracion tiempo={data.duracion} />
-                <Dias dias={data.dias} />
-                <Inscripcion inscripcion={data.inscripcion} />
-                <Certificacion certificados={data.certificacion} />
+                {loading ? (
+                    <Spinner />
+                ) : data ? (
+                    <>
+                        <h1>{data.titulo}</h1>
+                        {data.presentacion ? <Presentacion texto={data.presentacion} /> : null}
+                        <Contenidos contenidos={data.contenidos} />
+                        <Requisitos requisitos={data.estudios} />
+                        <Duracion tiempo={data.duracion} />
+                        <Dias dias={data.dias} />
+                        <Inscripcion inscripcion={data.inscripcion} />
+                        <Certificacion certificados={data.certificacion} />
+                    </>
+                ) : (
+                    <h3>No hay información para el curso</h3>
+                )}
             </Section>
         </>
     );
