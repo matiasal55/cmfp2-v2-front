068e2a5e531794421d0decfcea8508257fea1b7c
Matias <matiasalarcon55@hotmail.com>
2021-07-14 17:22:05 -0300

    Obtencion de cursos a traves de la API

--- src/pages/cursos/CursosEspecialidades.jsx
+++ src/pages/cursos/CursosEspecialidades.jsx
@@ -1,16 +1,28 @@
 import Main from '../../components/Main';
 import Section from '../../components/Section';
 import background from '../../assets/background/especialidades.jpg';
-import cursos from '../../components/cursos/especialidades.json';
+// import cursos from '../../components/cursos/especialidades.json';
 import Lista from '../../components/cursos/Lista';
 import { useState, useEffect } from 'react';
 import Spinner from '../../components/Spinner';
+import { getHandler } from '../../utils/requestHandler';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 const CursosEspecialidades = () => {
     const [loading, setLoading] = useState(true);
+    const [cursos, setCursos] = useState([]);
+
+    const prueba = async () => {
+        try {
+            const lista = await getHandler('/cursos/especialidades');
+            setCursos(lista);
+        } catch (e) {
+            setCursos([]);
+        }
+    };
 
     useEffect(() => {
+        prueba();
         setTimeout(() => {
             setLoading(false);
         }, 3000);
