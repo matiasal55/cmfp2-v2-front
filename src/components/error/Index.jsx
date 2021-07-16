3558da1ffdd3d2aea36b3ec33302abefab71855b
Matias <matiasalarcon55@hotmail.com>
2021-07-15 21:28:41 -0300

    Componente que gestiona que tipo de error es y renderiza el componente indicado

---
+++ src/components/error/Index.jsx
@@ -0,0 +1,9 @@
+import Internal from './Internal';
+import NotFound from '../NotFound';
+
+const Index = ({ error }) => {
+    if (error === 400) return <NotFound />;
+    return <Internal />;
+};
+
+export default Index;
