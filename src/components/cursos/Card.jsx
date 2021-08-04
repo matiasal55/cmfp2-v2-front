01147d7f142ffb9996dab961eab29716282a2fe0
Matias <matiasalarcon55@hotmail.com>
2021-08-03 22:59:14 -0300

    Comentario borrado

--- src/components/cursos/Card.jsx
+++ src/components/cursos/Card.jsx
@@ -7,7 +7,6 @@ const Card = ({ curso }) => {
     return (
         <div className='cursos-card'>
             <Link to={`${path}/${curso.id}-${curso.titulo.toLowerCase().replace(/ /g, '-')}`}>
-                {/* <img src={curso.thumbnail} className='imgCurso' alt={curso.titulo} /> */}
                 <div className='imgCurso' style={{ backgroundImage: `url(${curso.thumbnail})` }} />
                 <div className='titulo'>
                     <span>{curso.titulo}</span>
