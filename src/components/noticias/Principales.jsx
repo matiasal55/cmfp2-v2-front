2a6002250b8151975295825be9440429b2e7cafe
Matias <matiasalarcon55@hotmail.com>
2021-08-03 23:42:08 -0300

    Obtener ultimas noticias desde API. Manejo de errores

--- src/components/noticias/Principales.jsx
+++ src/components/noticias/Principales.jsx
@@ -1,14 +1,39 @@
-import noticias from './noticias.json';
+import { useEffect, useState } from 'react';
+import LoadingData from '../LoadingData';
+import { getUltimasNoticias } from './getNoticias';
 
 const Principales = () => {
+    const [noticias, setNoticias] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [errorMsg, setErrorMsg] = useState('No hay noticias por el momento');
+
+    const ultimasNoticias = async () => {
+        const lista = await getUltimasNoticias();
+        setLoading(false);
+        setNoticias(lista);
+    };
+
+    useEffect(() => {
+        ultimasNoticias();
+    }, []);
+
+    if (!noticias) {
+        setNoticias([]);
+        setErrorMsg('Hubo un problema interno. Intente más tarde. Disculpe las molestias');
+    }
+
     return (
         <>
-            {noticias.slice(noticias.length - 3).map((noticia) => (
-                <div key={noticia.id} className='noticia-index'>
-                    <h3>{noticia.title}</h3>
-                    <p>{noticia.subtitle}</p>
-                </div>
-            ))}
+            <LoadingData loading={loading} condition={noticias && noticias.length > 0} message={errorMsg}>
+                {noticias
+                    ? noticias.map((noticia) => (
+                          <div key={noticia.id} className='noticia-index'>
+                              <h3>{noticia.title}</h3>
+                              <p>{noticia.subtitle}</p>
+                          </div>
+                      ))
+                    : null}
+            </LoadingData>
         </>
     );
 };
