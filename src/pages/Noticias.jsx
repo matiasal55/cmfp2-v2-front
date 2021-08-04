352d9a748acbffda4d6d1a3f7b1b27ee29afc5e1
Matias <matiasalarcon55@hotmail.com>
2021-08-03 22:54:15 -0300

    Array de noticias vacio

--- src/pages/Noticias.jsx
+++ src/pages/Noticias.jsx
@@ -1,18 +1,22 @@
 import Main from '../components/Main';
 import Section from '../components/Section';
-import noticias from '../components/noticias/noticias.json';
+// import noticias from '../components/noticias/noticias.json';
 import Card from '../components/noticias/Card';
 import background from '../assets/background/noticias.jpg';
 
 const Noticias = () => {
+    const noticias = [];
+
     return (
         <>
             <Main bgImage={background} />
             <Section>
                 <h1>Noticias</h1>
-                {noticias.map((noticia, index) => (
-                    <Card key={index} noticia={noticia} />
-                ))}
+                {noticias && noticias.length > 0 ? (
+                    noticias.map((noticia, index) => <Card key={index} noticia={noticia} />)
+                ) : (
+                    <h3>No hay noticias por el momento</h3>
+                )}
             </Section>
         </>
     );
