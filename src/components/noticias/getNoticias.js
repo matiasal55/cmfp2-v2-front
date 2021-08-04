5d477f02c14289891f31655a977109f2c8ee5f90
Matias <matiasalarcon55@hotmail.com>
2021-08-03 23:13:34 -0300

    Funcion generica de getData y getUltimasNoticias

--- src/components/noticias/getNoticias.js
+++ src/components/noticias/getNoticias.js
@@ -1,6 +1,16 @@
 import { getHandler } from '../../utils/requestHandler';
 
 export const getTodasNoticias = async () => {
-    const noticias = await getHandler('/noticias/all');
+    const noticias = await getData('all');
     return noticias;
+};
+
+export const getUltimasNoticias = async () => {
+    const noticias = await getData('last');
+    return noticias;
+};
+
+const getData = async (section) => {
+    const data = await getHandler('/noticias/' + section);
+    return data;
 };
